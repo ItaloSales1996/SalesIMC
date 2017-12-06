e09a91ce5a09efbadfa9d7c3f4985e294a3da554
Italo Sales <italo.sales10@gmail.com>
2017-12-06 04:01:54 -0200

    Adicionei o projeto alfa

---
+++ app/src/main/java/italo/adsv/salesimc/imc.java
@@ -0,0 +1,13 @@
+package italo.adsv.salesimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class imc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
